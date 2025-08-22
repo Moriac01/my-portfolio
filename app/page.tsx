3f4141cee02101a720af90818925ed8cac9ddbd3
Moriac KPANGAM <moriackpangam@gmail.com>
2025-08-22 13:08:02 +0100

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,64 @@
+"use client"
+
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {FiDownload} from "react-icons/fi"
+import Social from "@/components/Social";
+import { SheetFooter } from "@/components/ui/sheet";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <section className=" pt-30  lg:pt-40">
+      <div className=" container ">
+        <div className="flex flex-col lg:flex-row text-center items-center justify-between gap-6">
+          <div className="items-center lg:text-left order-2 lg:order-none ">
+            <span className=" text-white/60 font-medium tracking-tighter ">Developpeur Web & Designer</span>
+            <h1 className=" lg:text-4xl text-2xl font-extrabold font-serif lg:pt-4 lg:pb-4 ">Salut 🖖, Je suis  <br/> <span className="text-cyan-500">Moriac KPANGAM</span> </h1>
+            <p className="leading-5 tracking-tighter lg:tracking-wide font-medium text-xs text-white/80">Passionné par la création d'interfaces modernes et éfficaces. <br /> Depuis 2ans, j'explore le développement web et design pour <br /> transformer mes idées en projets concrets.</p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+            {/* btn & Social */}
+            <div className="pt-4 lg:pt-8 items-center flex gap-6 flex-col lg:flex-row">
+              <Button
+              variant="outline"
+              className="uppercase rounded-full  "
+              >
+                <span>Download CV</span>
+                <FiDownload/>
+              </Button>
+
+              <div className="">
+                <Social/>
+              </div>
+            </div>
+          </div>
+
+          {/*Photo */}
+          <div className=" order-1 lg:order-none text-center ">
+
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+            src="/photo.jpg"
+            alt="phooto profil"
+            className=" rounded-full  bg-gray-600 text-gray-500 grayscale m-4 hover:animate-pulse duration-500  lg:w-70 lg:h-85"
+            width={200}
+            height={200}
+            priority
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      <SheetFooter className="text-[10px] lg:text-xs text-center pt-15 pb-0.5 ">
+        <div className="flex justify-between px-6 lg:pt-7 pt-2 lg:px-38 ">
+          <div>By moriac Kpangam</div>
+          <div>
+            <ul className="flex gap-4">
+              <li><Link href="mailto:moriackpangam@gmail.com">Email</Link></li>
+              <li><Link href="">Github</Link></li>
+            </ul>
+          </div>
+        </div>
+      </SheetFooter>
+    </section>
+
   );
 }
